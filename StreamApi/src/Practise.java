6ad87265f8bc154fa52acbe3aacebdbf88253c8e
bikshapathi-r <133085231+bikshapathi-r@users.noreply.github.com>
2023-07-19 12:21:48 +0530

    Add files via upload

---
+++ StreamApi/src/Practise.java
@@ -0,0 +1,37 @@
+/*import java.lang.System.Logger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class PractiseStream<T> {
+	Set<Object> T;
+
+	public Set<Object> getT() {
+		return T;
+	}
+
+	public void setT(Set<Object> t) {
+		T = t;
+	}
+
+} */
+
+/*public class Practise {
+	
+	public static void main(String[] args) {
+		final Logger logger = Logger.getLogger(Practise.class);  
+		List<Integer> numbers = Arrays.asList(2, 3, 4, 5, 2);
+		Set<Integer> squareSet;
+		// collect method returns a set
+		try {
+		squareSet = numbers.stream().map(x -> x / 0).collect(Collectors.toSet());
+		}
+		catch(ArithmeticException e) {
+System.out.println("\n divid by Zero Not Allowed");	}
+}} */
+
+
+
+  
+  
