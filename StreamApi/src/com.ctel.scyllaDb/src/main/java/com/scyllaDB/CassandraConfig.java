e1c3a1f40be5b9d9c5bcfc1743a2e9cbe7d766dd
bikshapathi-r <133085231+bikshapathi-r@users.noreply.github.com>
2023-09-12 15:16:31 +0530

    Add files via upload
    ScyllaDB Implementation.....

---
+++ StreamApi/src/com.ctel.scyllaDb/src/main/java/com/scyllaDB/CassandraConfig.java
@@ -0,0 +1,31 @@
+package com.scyllaDB;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.core.mapping.BasicCassandraMappingContext;
+import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
+
+@Configuration
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Override
+    protected String getKeyspaceName() {
+        return "my_keyspace";
+    }
+
+    @Bean
+    public CassandraClusterFactoryBean cluster() {
+        CassandraClusterFactoryBean cluster = 
+          new CassandraClusterFactoryBean();
+        cluster.setContactPoints("127.0.0.1");
+        cluster.setPort(9142);
+        return cluster;
+    }
+
+    @Bean
+    public CassandraMappingContext cassandraMapping() 
+      throws ClassNotFoundException {
+        return new BasicCassandraMappingContext();
+    }
+}
