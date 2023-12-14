d7c041405e4558c35f450c566147a844b1ee84d8
bikshapathi-r <133085231+bikshapathi-r@users.noreply.github.com>
2023-12-14 16:40:26 +0530

    Delete HpfsProducerConsumer directory

--- HpfsProducerConsumer/src/main/java/com/ctel/hpfs/Repositary/PayloadRepositary.java
+++
@@ -1,9 +0,0 @@
-package com.ctel.hpfs.Repositary;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.ctel.hpfs.entity.Shipments;
-
-public interface PayloadRepositary extends MongoRepository<Shipments,String> {
-
-}
